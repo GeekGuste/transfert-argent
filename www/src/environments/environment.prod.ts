8315156a25978593f4fbfa51c32bb1efd96ec323
alfayedtch <albos7484@gmail.com>
2025-11-23 15:08:40 +0100

    update add home button on maintenance page

--- www/src/environments/environment.prod.ts
+++ www/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   endpoint:'http://194.164.60.134:8082',
   endpointForImage:'http://194.164.60.134:8082/storage/',
-  version: '1.0.2'
+  version: '1.0.3'
 
 };
--- www/src/environments/environment.ts
+++ www/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   endpoint:'http://194.164.60.134:8082',
   endpointForImage:'http://194.164.60.134:8082/storage/',
-  version: '1.0.2'
+  version: '1.0.3'
 };
