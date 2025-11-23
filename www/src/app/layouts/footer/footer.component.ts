e1dedc0248386d1e2398637650a4b813b70b8db1
alfayedtch <albos7484@gmail.com>
2025-11-23 14:47:13 +0100

    add version on footer

--- www/src/app/layouts/footer/footer.component.ts
+++ www/src/app/layouts/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core'
 import { credits, currentYear } from '../../common/constants'
-
+import { environment } from '../../../environments/environment'
 @Component({
   selector: 'app-footer',
   standalone: true,
@@ -11,4 +11,5 @@ import { credits, currentYear } from '../../common/constants'
 export class FooterComponent {
   currentYear = currentYear;
   credits = credits
+  appVersion = environment.version; // You can set this dynamically if needed
 }
